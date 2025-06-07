403502e4143c13b3124d129c658e7e58499bc249
GiangNT <giangntse150746@gmail.com>
2025-06-07 13:15:03 +0700

    feat(src/): add qr code generate page

---
+++ src/pages/demo/code-generator.tsx
@@ -0,0 +1,6 @@
+import QRGenerator from '@/components/shared/generator/qr-generator'
+
+export default function CodeGeneratePage() {
+  // package using: qrcode.react
+  return <QRGenerator />
+}
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -7,7 +7,8 @@ import HomePage from '@/pages/home'
 import MaintenancePage from '@/pages/system/maintanance'
 import NotFoundPage from '@/pages/system/not-found'
 import DemoPage from '@/pages/demo'
-import ScannerPage from '@/pages/demo/scanner'
+import QRScanPage from '@/pages/demo/code-scanner'
+import CodeGeneratePage from '@/pages/demo/code-generator'
 
 type RouteGroup = {
   layout: JSX.Element
@@ -16,24 +17,28 @@ type RouteGroup = {
 
 export const ROUTES: RouteGroup[] = [
   {
-    layout: <BlankLayout />,
+    layout: <DefaultLayout />,
     pages: [
-      {
-        path: '/demo',
-        element: <DemoPage />,
-        children: [
-          { id: 'demo-scanner', path: 'scanner', element: <ScannerPage /> },
-          // { path: "profile", element: <Profile /> },
-        ],
-      },
+      { id: 'home', path: '/', element: <HomePage /> },
+      { id: 'maintenance', path: '/maintenance', element: <MaintenancePage /> },
+      { id: 'not-found', path: '/not-found', element: <NotFoundPage /> },
     ],
   },
   {
-    layout: <DefaultLayout />,
+    layout: <BlankLayout />,
     pages: [
-      { path: '/', element: <HomePage /> },
-      { path: '/maintenance', element: <MaintenancePage /> },
-      { path: '/not-found', element: <NotFoundPage /> },
+      {
+        id: 'demo',
+        path: '/demo',
+        // element: <DemoPage />,
+        children: [
+          { id: 'demo-list', path: '', element: <DemoPage /> },
+          { id: 'qr-scan', path: 'code-scanner', element: <QRScanPage /> },
+          { id: 'qr-generate', path: 'code-generator', element: <CodeGeneratePage /> },
+          // { id: 'demo-not-found', path: '*', element: <NotFoundPage /> },
+          // { path: "profile", element: <Profile /> },
+        ],
+      },
     ],
   },
 ]
